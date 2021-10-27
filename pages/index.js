99dcc830c1d2681194c64bb778e02fcefb0460eb
Challa Sharath <csharath@outlook.com>
2021-10-27 12:39:46 +0530

    bump nextjs version

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,6 @@ export default function Home() {
         <title>Storyflow.video</title>
         <meta name="description" content="Add a quick video widget to your website." />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet" />
       </Head>
 
       <header className={styles.header}>

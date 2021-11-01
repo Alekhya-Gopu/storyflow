b7eda5c3d52892ff821d884fb2f774f2054aa43d
Challa Sharath <csharath@outlook.com>
2021-11-01 16:01:17 +0530

    add meta tags for fb and twitter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
@@ -10,9 +11,30 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+            <title>Storyflow</title>
+            <meta name="description" content="Improve storytelling by just adding a video widget to your website in seconds." />
+            <meta property="og:title" content="Storyflow" />
+            <meta property="og:description" content="Immersive storytelling redefined for web." />
+            <meta property="og:image" content="/storyflow-meta.svg" />
+            <meta property="og:url" content="https://storyflow.video" />
+          
+            {/* Open Graph / Facebook */}
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://storyflow.video/" />
+            <meta property="og:title" content="Storyflow" />
+            <meta property="og:description" content="Improve storytelling by just adding a video widget to your website in seconds." />
+            <meta property="og:image" content="/storyflow-meta.svg" />
+
+            {/* Twitter */}
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://storyflow.video/" />
+            <meta property="twitter:title" content="Storyflow" />
+            <meta property="twitter:description" content="Improve storytelling by just adding a video widget to your website in seconds." />
+            <meta property="twitter:image" content="/storyflow-meta.svg" />
+            <link rel="icon" href="/storyflow-favicon.svg" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Cabin&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Cabin&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />

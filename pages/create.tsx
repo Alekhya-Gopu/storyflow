7fae5c05ba4d70c2bfe4b6a4996c6d26134c46ed
Challa Sharath <csharath@outlook.com>
2021-11-04 23:52:27 +0530

    save stories to supabase

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,21 +1,68 @@
 import { supabase } from '@supabase/client';
 import { GetServerSideProps } from 'next';
+import { useState } from 'react';
 import { User } from '@supabase/supabase-js';
 import styles from '@styles/Create.module.css';
 import Input from '@components/Input';
 import Button from '@components/Button';
 import Icon from '@components/Icon';
 
-export default function Create(user: User) {
+interface CreateProps {
+    user: User;
+};
+
+export default function Create({ user }: CreateProps) {
+    const [saving, setSaving] = useState<boolean>(false);
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [storyUrl, setStoryUrl] = useState<string>('');
+
+    const saveStory = async () => {
+        setSaving(true);
+        const story = {
+            story_name: title,
+            story_desc: description,
+            story_url: storyUrl,
+            user_uid: user.id,
+        };
+
+        const { data, error } = await supabase.from('user_stories').insert(story);
+        setSaving(false);
+        if (error) {
+            console.error(error);
+        }
+        console.log(data);
+    };
+
     return (
         <div className={styles.create}>
             <h3>Add your Story Videos</h3>
             <div className={styles.inputs}>
-                <Input placeholder="Title" icon="edit-3" />
-                <Input placeholder="Description" icon="edit-3" />
-                <Input type="url" placeholder="Video URL" icon="link" />
-                <Button size="large">
-                    <Icon type="plus-circle" />Add
+                <Input
+                    placeholder="Title"
+                    icon="edit-3"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <Input
+                    placeholder="Description"
+                    icon="edit-3"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <Input
+                    type="url"
+                    placeholder="Video Story URL"
+                    icon="link"
+                    value={storyUrl}
+                    onChange={(e) => setStoryUrl(e.target.value)}
+                />
+                <Button
+                    size="large"
+                    disabled={saving}
+                    onClick={saveStory}>
+                    <Icon type="plus-circle" />
+                    <span>{saving ? 'Saving...' : 'Save'}</span>
                 </Button>
             </div>
         </div>

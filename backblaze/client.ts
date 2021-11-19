fe0410193dd485cffc13c3e9ed28b844ac945eeb
Challa Sharath <csharath@outlook.com>
2021-11-19 13:35:02 +0530

    fix typescript errors

--- backblaze/client.ts
+++ backblaze/client.ts
@@ -1,9 +1,9 @@
 import B2 from 'backblaze-b2';
 
 const b2 = new B2({
-    apiUrl: 'https://api.backblazeb2.com/b2api/v2',
-    applicationKeyId: process.env.BACKBLAZE_APP_ID,
-    applicationKey: process.env.BACKBLAZE_APP_KEY
+    apiUrl: 'https://api.backblazeb2.com/b2api/v2' as string,
+    applicationKeyId: process.env.BACKBLAZE_APP_ID as string,
+    applicationKey: process.env.BACKBLAZE_APP_KEY as string
 });
 
 export default b2;

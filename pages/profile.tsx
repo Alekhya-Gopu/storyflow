4acd8d679d965afcab911d3dccbc53810700af56
Challa Sharath <csharath@outlook.com>
2021-11-20 00:25:18 +0530

    polish profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,10 @@
+import styles from '@styles/Profile.module.css';
 import { useState, useEffect } from 'react';
 import { supabase } from '@supabase/client';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Button from '@components/Button';
 import { User } from '@supabase/supabase-js';
 
 const Profile: NextPage = () => {
@@ -28,10 +31,12 @@ const Profile: NextPage = () => {
     if (!profile) return null;
 
     return (
-        <div style={{ maxWidth: '420px', margin: '96px auto' }}>
-            <h2>Hello, {profile.email}</h2>
-            <p>User ID: {profile.id}</p>
-            <button onClick={signOut}>Sign Out</button>
+        <div className={styles.profile}>
+            <Image src="/personal-info.svg" alt="Storyflow illustration" width={650} height={400} />
+            <div>
+                <h2>Hello, {profile.email}</h2>
+                <Button size="large" onClick={signOut}>Logout</Button>
+            </div>
         </div>
     )
 }

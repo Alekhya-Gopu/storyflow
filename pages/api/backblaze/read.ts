72eb7b7e20d984c672886c22ba1a7b595a591499
Challa Sharath <csharath@outlook.com>
2021-11-19 13:15:33 +0530

    fix file reading

--- pages/api/backblaze/read.ts
+++ pages/api/backblaze/read.ts
@@ -1,14 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import b2 from '@backblaze/client';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function getFileData(id: string) {
     try {
-        // get user Id
-        const { id } = req.query;
-        if (!id) {
-            throw new Error('No user id provided');
-        }
-
         // must authorize first (authorization lasts 24 hrs)
         await b2.authorize();
 
@@ -20,8 +14,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 console.log(progress);
             }
         });
+        return data;
+    } catch (error) {
+        return error;
+    }
 
-        return res.send(data);
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        // get user Id
+        const { id } = req.query;
+        if (!id) {
+            throw new Error('No user id provided');
+        }
+
+        const data = await getFileData(id as string);
+        res.status(200).send(data);
     } catch (err) {
         res.status(500).json({
             message: 'Failed to upload file',
--- pages/api/render-story.ts
+++ pages/api/render-story.ts
@@ -1,15 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import read from './backblaze/read';
+import { getFileData } from './backblaze/read';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const data = await read(req, res);
-
+        const { id } = req.query;
+        const data = await getFileData(id as string);
         res.setHeader(
             'Content-Type', 'text/html'
         );
-        const html = await data?.text();
-        res.status(200).send(html);
+        res.status(200).send(data);
     } catch (err) {
         res.status(500).json({ err });
     }

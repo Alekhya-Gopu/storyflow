95a35be6cefd20dcb9a4d3137c2deceb734e9cbc
Challa Sharath <csharath@outlook.com>
2021-11-25 11:36:59 +0530

    update new fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,23 +25,7 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/storyflow-favicon.svg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Cabin&display=swap" rel="stylesheet" />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />

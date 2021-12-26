0cb51a769ce807c7ae7795ab5f7efdbfcbbfcc94
Challa Sharath <csharath@outlook.com>
2021-12-26 18:50:48 +0530

    fix stale closure

--- components/Story/index.tsx
+++ components/Story/index.tsx
@@ -1,6 +1,6 @@
 import styles from './Story.module.css';
 import Icon from '@components/Icon';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import cn from 'classnames';
 
 interface StoryProps {
@@ -12,17 +12,21 @@ interface StoryProps {
 function Story({ className, children, showNavigation = false }: StoryProps) {
     const [currentPage, setCurrentPage] = useState<number>(0);
     const [mount, setMount] = useState<boolean>(false);
-    const [pages] = useState<HTMLLIElement[]>(Array.from(children as HTMLCollectionOf<HTMLLIElement>));
+    const pages = useRef<HTMLLIElement[]>(Array.from(children as HTMLCollectionOf<HTMLLIElement>));
 
     const navigateTo = (direction: 'next' | 'prev' | number): void => {
         if (typeof direction === 'number') {
             setCurrentPage(direction);
         } else {
             if (direction === 'next') {
-                pages.length - 1 > currentPage && setCurrentPage((prev) => prev + 1);
+                pages.current.length - 1 > currentPage && setCurrentPage((prev) => prev + 1);
             } else {
                 currentPage > 0 && setCurrentPage((prev) => prev - 1);
             }
+        }
+
+        if (currentPage === pages.current.length - 1) {
+            setCurrentPage(0);
         }
     }
 
@@ -30,25 +34,26 @@ function Story({ className, children, showNavigation = false }: StoryProps) {
         setMount(true);
 
         // start playing story automatically
-        // const timer = setInterval(() => {
-        //     navigateTo('next');
-        // }, 8000);
+        const timer = setInterval(() => {
+            navigateTo('next');
+        }, 8000);
 
-        // return () => { clearInterval(timer) }
-    }, []);
+        return () => { clearInterval(timer) }
+    }, [currentPage]);
 
     return (
         <div className={cn(styles.story, `${className}`)}>
             <ul className={styles.bars}>
-                {mount && pages.map((_, i) => (
+                {mount && pages.current.map((_, i) => (
                     <li key={i} onClick={() => navigateTo(i)} className={cn(styles.bar, { [styles.active]: i === currentPage })}>
                         <div className={styles.barInner} />
-                    </li>))}
+                    </li>
+                ))}
             </ul>
             <div className={styles.content}>
                 {showNavigation && <Icon type="chevron-left" size={48} onClick={() => navigateTo('prev')} />}
                 <ul className={styles.pages}>
-                    {pages.filter((_, i) => i === currentPage)}
+                    {pages.current.filter((_, i) => i === currentPage)}
                 </ul>
                 {showNavigation && <Icon type="chevron-right" size={48} onClick={() => navigateTo('next')} />}
             </div>

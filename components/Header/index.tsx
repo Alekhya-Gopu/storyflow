03c6001954421a8965f18a454c1e4bc24289e788
Challa Sharath <csharath@outlook.com>
2021-12-30 19:41:00 +0530

    add feedback

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from './Header.module.css';
-import Icon from '@components/Icon';
 import Feedback from '@components/Feedback';
 import MenuIcon from '@components/MenuIcon';
 import Link from 'next/link';
@@ -40,12 +39,12 @@ export default function Header({ authState, email }: HeaderProps) {
           <li className={styles.navItem}>
             <a href="https://calendly.com/src200" target="_blank" rel="noreferrer">Schedule a demo</a>
           </li>
-          {/* <li className={styles.navItem}>
+          <li className={styles.navItem}>
             <a className={styles.feedbackItem}>Feedback</a>
             <div className={styles.feedbackForm}>
               <Feedback />
             </div>
-          </li> */}
+          </li>
         </ul>
       </nav>
       {authState === 'authenticated' ? (

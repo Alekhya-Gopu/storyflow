7667c6c3476df5c9e7e72a0ad80c061ba8999e71
Challa Sharath <csharath@outlook.com>
2021-12-30 14:20:29 +0530

    featured on product hunt

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,6 +5,9 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       &copy; 2021 &nbsp; | &nbsp; Storyflow
+      <a href="https://www.producthunt.com/posts/storyflow?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-storyflow" target="_blank" rel="noreferrer">
+        <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=324939&theme=light" alt="Storyflow - A simple web story CMS platform | Product Hunt" width="200" height="44" />
+      </a>
     </footer>
   )
 }
